b8315565557eaa844a59db53ea13f16e9daf1cdc
sofiacottone <cottonesofia@outlook.it>
2024-04-17 14:49:45 +0200

    feat: add header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+
+            <a class="navbar-brand text-danger fw-bold text-uppercase" href="#">Boolfix</a>
+
+            <div class="d-flex" role="search">
+                <input class="form-control btn-outline-danger me-2" type="search" placeholder="Search"
+                    aria-label="Search">
+                <button class="btn btn-outline-danger" type="submit">Search</button>
+            </div>
+
+        </div>
+    </nav>
+</template>
+<style lang="scss">
+@use '../src/style/generic.scss' as *;
+
+nav {
+    .navbar-brand {
+        text-shadow: 1px 1px #ffffff60;
+    }
+}
+</style>
