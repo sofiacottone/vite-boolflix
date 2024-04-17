f7bc43ec4e3e45f00592521a6ac76d4bc85327bb
sofiacottone <cottonesofia@outlook.it>
2024-04-17 16:04:30 +0200

    feat: import single card component

---
+++ src/components/AppMainCards.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from '../store.js'
+import SingleCard from './SingleCard.vue'
+
+export default {
+    name: 'AppMainCards',
+    components: {
+        SingleCard
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="d-flex flex-wrap justify-content-start gap-4 text-bg-secondary p-3">
+        <SingleCard v-for="movie in store.movies" :movie-info="movie"></SingleCard>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
