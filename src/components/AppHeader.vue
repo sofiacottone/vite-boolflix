42f604f6078cc4a6f4baeaa9912d11d6f3e82319
sofiacottone <cottonesofia@outlook.it>
2024-04-18 16:24:15 +0200

    feat: add minor style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,26 +12,41 @@ export default {
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg bg-dark">
-        <div class="container-fluid">
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container-fluid">
 
-            <a class="navbar-brand text-danger fw-bold text-uppercase" href="#">Boolflix</a>
+                <a class="navbar-brand text-danger fw-bold text-uppercase" href="#">Boolflix</a>
 
-            <div class="d-flex" role="search">
-                <input v-model="store.searchedMovie" class="form-control btn-outline-danger me-2" type="search"
-                    placeholder="Search" aria-label="Search">
-                <button @click="$emit('searchPerformed')" class="btn btn-outline-danger" type="submit">Search</button>
+                <div class="d-flex" role="search">
+                    <input v-model="store.searchedMovie" class="form-control btn-outline-danger me-2" type="search"
+                        placeholder="Search" aria-label="Search">
+                    <button @click="$emit('searchPerformed', 'searchSeriesPerformed')" class="btn btn-outline-danger"
+                        type="submit">Search</button>
+                </div>
+
             </div>
-
-        </div>
-    </nav>
+        </nav>
+    </header>
 </template>
 <style scoped lang="scss">
 @use '../src/style/generic.scss' as *;
 
-nav {
-    .navbar-brand {
-        text-shadow: 1px 1px #ffffff60;
+header {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 99;
+    box-shadow: 1px 1px #ffffff40;
+
+    nav {
+        min-height: 70px;
+
+        .navbar-brand {
+            text-shadow: 1px 1px #ffffff60;
+        }
     }
+
 }
 </style>
