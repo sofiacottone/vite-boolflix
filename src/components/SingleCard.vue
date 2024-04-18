4b21c648c55a00fef22f43cdc103365d61069663
sofiacottone <cottonesofia@outlook.it>
2024-04-18 16:15:37 +0200

    feat: print dynamic content

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -18,20 +18,25 @@ export default {
 <template>
 
     <!-- single card  -->
-    <div class="ms-flip-card bg-dark">
+    <div class="ms-flip-card bg-dark border">
+
         <div class="ms-flip-card-inner">
+
             <div class="ms-flip-card-front">
-                <img src="https://image.tmdb.org/t/p/w342/cNYYPcbcfT90wTyI6hUmJH5Fx8H.jpg" alt="">
+                <!-- <img src="https://image.tmdb.org/t/p/w342/cNYYPcbcfT90wTyI6hUmJH5Fx8H.jpg" alt=""> -->
             </div>
-            <div class="ms-flip-card-back">
-                <div>{{ movieInfo.title }}</div>
-                <div>{{ movieInfo.original_title }}</div>
+
+            <div class="ms-flip-card-back p-3">
+                <div><span class="text-danger">Title: </span>{{ movieInfo.title || movieInfo.name }}</div>
+                <div><span class="text-danger">Original Title: </span>{{ movieInfo.original_title ||
+                    movieInfo.original_name }}</div>
                 <div class="ms-flag-wrapper d-flex align-items-center gap-2">
-                    <div>{{ movieInfo.original_language }}</div>
+                    <div><span class="text-danger">Original Language: </span> {{ movieInfo.original_language }}</div>
                     <img :src="getFlag()" onerror='this.style.display = "none"'>
                 </div>
-                <div>{{ Math.floor(movieInfo.vote_average / 2) }}</div>
+                <div><span class="text-danger">Rating: </span> {{ Math.floor(movieInfo.vote_average / 2) }}</div>
             </div>
+
         </div>
     </div>
 
@@ -40,6 +45,7 @@ export default {
 <style scoped lang="scss">
 .ms-flip-card {
     width: calc(100% / 3 - 20px);
+    min-height: 360px;
 
     img {
         width: 100%;
