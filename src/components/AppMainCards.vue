4e59fdff1f8381607030b7485417694ed70abb85
sofiacottone <cottonesofia@outlook.it>
2024-04-18 16:16:49 +0200

    feat: import single card component + loop to get content from APIs

--- src/components/AppMainCards.vue
+++ src/components/AppMainCards.vue
@@ -5,7 +5,7 @@ import SingleCard from './SingleCard.vue'
 export default {
     name: 'AppMainCards',
     components: {
-        SingleCard
+        SingleCard,
     },
     data() {
         return {
@@ -16,8 +16,20 @@ export default {
 </script>
 
 <template>
-    <div class="d-flex flex-wrap justify-content-start gap-4 text-bg-secondary p-3">
-        <SingleCard v-for="movie in store.movies" :movieInfo="movie" :key="movie.id"></SingleCard>
+    <div class="text-bg-secondary p-3 mt-5">
+        <h3 class="fw-bold pt-3 pb-1 ps-1">Films</h3>
+        <div class="d-flex flex-wrap justify-content-start gap-4 text-bg-secondary">
+            <SingleCard v-for="movie in store.movies" :movieInfo="movie" :key="movie.id">
+            </SingleCard>
+        </div>
+
+        <h3 class="fw-bold pt-4 pb-1 ps-1">TV Series</h3>
+        <div class="d-flex flex-wrap justify-content-start gap-4 text-bg-secondary">
+            <SingleCard v-for="series in store.series" :movieInfo="series" :key="series.id">
+            </SingleCard>
+        </div>
+
+
     </div>
 </template>
 
