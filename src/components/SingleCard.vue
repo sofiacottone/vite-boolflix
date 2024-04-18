0e9596b201ffa2218229113b433c5e3d51f0ced1
sofiacottone <cottonesofia@outlook.it>
2024-04-18 16:48:50 +0200

    feat: add dynamic images

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -8,6 +8,10 @@ export default {
         getFlag() {
             let flag = `/src/assets/img/flags/${this.movieInfo.original_language}.png`;
             return flag;
+        },
+        getCardImage(movieInfo) {
+            let cardImage = `https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`;
+            return cardImage;
         }
     },
     mounted() {
@@ -23,18 +27,23 @@ export default {
         <div class="ms-flip-card-inner">
 
             <div class="ms-flip-card-front">
-                <!-- <img src="https://image.tmdb.org/t/p/w342/cNYYPcbcfT90wTyI6hUmJH5Fx8H.jpg" alt=""> -->
+                <img :src="getCardImage(movieInfo)" :alt="movieInfo.title || movieInfo.name">
             </div>
 
             <div class="ms-flip-card-back p-3">
                 <div><span class="text-danger">Title: </span>{{ movieInfo.title || movieInfo.name }}</div>
                 <div><span class="text-danger">Original Title: </span>{{ movieInfo.original_title ||
                     movieInfo.original_name }}</div>
-                <div class="ms-flag-wrapper d-flex align-items-center gap-2">
-                    <div><span class="text-danger">Original Language: </span> {{ movieInfo.original_language }}</div>
-                    <img :src="getFlag()" onerror='this.style.display = "none"'>
+                <div class="ms-flag-wrapper">
+                    <span class="text-danger">Original Language: </span>
+
+                    <div class="d-flex align-items-center gap-2">
+                        {{ movieInfo.original_language }}
+                        <img :src="getFlag()" onerror='this.style.display = "none"'>
+                    </div>
                 </div>
                 <div><span class="text-danger">Rating: </span> {{ Math.floor(movieInfo.vote_average / 2) }}</div>
+                <!-- <div>{{ getCardImage() }}</div> -->
             </div>
 
         </div>
