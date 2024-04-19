8d364b3b03cf79d19e05a71e2f967e22f158f20b
sofiacottone <cottonesofia@outlook.it>
2024-04-19 10:49:38 +0200

    fix: remove gap

--- src/components/AppMainCards.vue
+++ src/components/AppMainCards.vue
@@ -18,13 +18,13 @@ export default {
 <template>
     <div class="text-bg-secondary p-3 mt-5">
         <h3 class="fw-bold pt-3 pb-1 ps-1">Films</h3>
-        <div class="d-flex flex-wrap justify-content-start gap-4 text-bg-secondary">
+        <div class="d-flex flex-wrap justify-content-start text-bg-secondary">
             <SingleCard v-for="movie in store.movies" :movieInfo="movie" :key="movie.id">
             </SingleCard>
         </div>
 
         <h3 class="fw-bold pt-4 pb-1 ps-1">TV Series</h3>
-        <div class="d-flex flex-wrap justify-content-start gap-4 text-bg-secondary">
+        <div class="d-flex flex-wrap justify-content-start text-bg-secondary">
             <SingleCard v-for="series in store.series" :movieInfo="series" :key="series.id">
             </SingleCard>
         </div>
