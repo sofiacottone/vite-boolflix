9c776f8f769995339622123a1338b6e9fdee7626
sofiacottone <cottonesofia@outlook.it>
2024-04-20 11:22:15 +0200

    feat: add placeholder in case of broken poster image

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -14,8 +14,8 @@ export default {
             let flag = `/src/assets/img/flags/${this.movieInfo.original_language}.png`;
             return flag;
         },
-        getCardImage(movieInfo) {
-            let cardImage = `https://image.tmdb.org/t/p/w342${movieInfo.poster_path}`;
+        getCardImage() {
+            let cardImage = `https://image.tmdb.org/t/p/w342${this.movieInfo.poster_path}`;
             return cardImage;
         },
         getRatingStars() {
@@ -38,12 +38,18 @@ export default {
 <template>
 
     <!-- single card  -->
-    <div class="ms-flip-card col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 bg-dark border mb-4">
+    <div class="ms-flip-card col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-9 mb-4 mx-auto">
 
-        <div class="ms-flip-card-inner">
+        <div class="ms-flip-card-inner border">
 
             <div class="ms-flip-card-front">
-                <img :src="getCardImage(movieInfo)" :alt="movieInfo.title || movieInfo.name">
+                <img v-if="movieInfo.poster_path" :src="getCardImage()" :alt="movieInfo.title || movieInfo.name">
+                <div class="ms-img-placeholder d-flex justify-content-center align-items-center text-uppercase text-center fw-bold fs-4"
+                    v-else>
+                    {{ movieInfo.title
+                    ||
+                    movieInfo.name
+                    }}</div>
             </div>
 
             <div class="ms-flip-card-back p-3">
@@ -74,34 +80,80 @@ export default {
 
 <style scoped lang="scss">
 .ms-flip-card {
-    // width: calc(100% / 3 - 20px);
-    min-height: 310px;
+    max-height: 800px;
+    background-color: transparent;
+    perspective: 1000px;
 
-    .ms-flip-card-front {
-        img {
-            width: 100%;
-            max-height: 100%;
-            min-height: 440px;
-            object-fit: cover;
-            object-position: center;
-            aspect-ratio: 2/3;
-        }
-
+    &:hover .ms-flip-card-inner {
+        transform: rotateY(180deg);
     }
 
-    .ms-flag-wrapper {
-        width: 30px;
-        height: 30px;
+    .ms-flip-card-inner {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        transition: transform 0.8s;
+        transform-style: preserve-3d;
+        color: #fff;
 
-        img {
+        .ms-flip-card-front {
             width: 100%;
-            max-height: 100%;
+            height: 100%;
+            backface-visibility: hidden;
+
+            img {
+                width: 100%;
+                max-height: 100%;
+                min-height: 440px;
+                object-fit: cover;
+                object-position: center;
+                aspect-ratio: 2/3;
+            }
+
+            .ms-img-placeholder {
+                background-color: #000;
+                color: #dc3444;
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        .ms-flip-card-back {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            backface-visibility: hidden;
+            transform: rotateY(180deg);
+            background-color: #000;
+
+            .ms-flag-wrapper {
+                width: 30px;
+                height: 30px;
+
+                img {
+                    width: 100%;
+                    max-height: 100%;
+                }
+            }
+
+            i {
+                &.fa-solid {
+                    color: #dc3444;
+                }
+            }
         }
     }
+}
 
-    i {
-        &.fa-solid {
-            color: #dc3444;
+@media screen and (max-width: 1200px) {
+    .ms-flip-card {
+        .ms-flip-card-inner {
+            .ms-flip-card-back {
+                font-size: 26px;
+
+            }
         }
     }
 }
